63ebe8734c163f64fa7c98dc622847856936020e
BRYT237 <diyaolutomiwa@gmail.com>
2025-11-30 22:32:15 +0100

    late

--- src/components/Pages/Dashboard.jsx
+++ src/components/Pages/Dashboard.jsx
@@ -16,7 +16,7 @@ const Dashboard = () => {
     <div className='block px-[10em] gap-[2em] my-[3em] sm:flex'>
        <div><FeaturedJobs /></div>
 
-        <div><SingleJobs /></div>
+        <div className='hidden sm:block'><SingleJobs /></div>
 
       </div>
 
